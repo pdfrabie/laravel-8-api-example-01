f664042f19aeb396f60b9e6277bfa89537868f73
pdfrabie <pdfrabie@gmail.com>
2023-06-09 12:13:27 +0200

    url to .env

--- app/Http/Controllers/BreweriesController.php
+++ app/Http/Controllers/BreweriesController.php
@@ -10,12 +10,12 @@ class BreweriesController extends Controller
     public function __construct(private Guzzle $guzzle, private Helper $helper){}
 
     public function index() {
-        $breweries = $this->guzzle->get('https://api.openbrewerydb.org/breweries');
+        $breweries = $this->guzzle->get(env('OPENBREWERY_LIST', false));
         return $this->helper->nestAddress($breweries);
     }
 
     public function search($name) {
-        $breweries = $this->guzzle->get('https://api.openbrewerydb.org/breweries?by_name='.$name);
+        $breweries = $this->guzzle->get(env('OPENBREWERY_LIST', false).'?by_name='.$name);
         return $this->helper->nestAddress($breweries);
     }
 }
