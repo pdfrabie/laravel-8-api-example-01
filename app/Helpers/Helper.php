282ecd6b9cb4266769101ad218eb0221f49479c4
pdfrabie <pdfrabie@gmail.com>
2023-06-16 12:11:07 +0200

    Using laravel collection instead

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -5,27 +5,26 @@ namespace App\Helpers;
 class Helper
 {
     public function nestAddress($data) {
-        $newStructure = [];
-
-        foreach ($data as $value) {
-            $newStructure[] = (object) [
-                'id' => $value->id,
-                'name' => $value->name,
-                'brewery_type' => $value->brewery_type,
-                'phone' => $value->phone,
-                'website_url' => $value->website_url,
-                'longitude' => $value->longitude,
-                'latitude' => $value->latitude,
-                'address' => (object) [
-                    'street' => $value->street,
-                    'city' => $value->city,
-                    'state_province' => $value->state_province,
-                    'state' => $value->state,
-                    'country' => $value->country,
-                    'postal_code' => $value->postal_code,
-                ]
-            ];
-        }
+        $newStructure = collect( $data )
+            ->map(function ($value) {
+                return (object) [
+                    'id' => $value->id,
+                    'name' => $value->name,
+                    'brewery_type' => $value->brewery_type,
+                    'phone' => $value->phone,
+                    'website_url' => $value->website_url,
+                    'longitude' => $value->longitude,
+                    'latitude' => $value->latitude,
+                    'address' => (object) [
+                        'street' => $value->street,
+                        'city' => $value->city,
+                        'state_province' => $value->state_province,
+                        'state' => $value->state,
+                        'country' => $value->country,
+                        'postal_code' => $value->postal_code,
+                    ]
+                ];
+            });
 
         return $newStructure;
     }
